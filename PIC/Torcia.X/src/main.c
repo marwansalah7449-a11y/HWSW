ab1c1b01776ad5f9891121d9995403a3c747b095
Salah Marwan <gvs448@cam.bellinzona>
2026-03-16 15:50:40 +0100

    Changed logic in Torch, first working version

--- PIC/Torcia.X/src/main.c
+++ PIC/Torcia.X/src/main.c
@@ -60,41 +60,55 @@ void FLASH(){
 
 
 void main(){
-	// init hardware
-	eh100_init();
+    eh100_init();
     DL1 = 0;
+    
+    uint8_t program = 0;  
     uint8_t lastSW1 = 1;
+    uint16_t SW1Counter = 0;
+    bool buttonReleased = false;
+    
     while (1) {
-        if (SW1 != lastSW1){
-            __delay_ms(50);
-            
-            while(SW1){
-                PRG1();
-            }
-            lastSW1 = SW1;
-            if(SW1 != lastSW1){
-                __delay_ms(50);
-                while(SW1){
-                    PRG2();
-                }
-                lastSW1 = SW1;
-                if(SW1 != lastSW1){
-                    __delay_ms(50);
-                    while(SW1){
-                        PRG3();
-                    }
-                    lastSW1 = SW1;
-                    if(SW1 != lastSW1){
-                        __delay_ms(50);
-                        while(SW1){
-                            FLASH();
-                        }
-                        lastSW1 = SW1;
+        
+        
+        if (SW1 == 0) { 
+            SW1Counter++;
+            buttonReleased = false;
+        } else {  
+            if (SW1Counter > 0 && !buttonReleased) {
+               
+                if (SW1Counter >= 100) {
+                    program = (program == 0) ? 1 : 0;  
+                } else {
+                    
+                    if (program != 0) {
+                        program = (program % 4) + 1;  
                     }
                 }
             }
+            SW1Counter = 0;
+            buttonReleased = true;
         }
         
-    }
+        
+        switch (program) {
+            case 0:
+                DL1 = 0;
+                break;
+            case 1:
+                PRG1();
+                break;
+            case 2:
+                PRG2();
+                break;
+            case 3:
+                PRG3();
+                break;
+            case 4:
+                FLASH();
+                break;
+        }
+        __delay_ms(10);
+      }
 }
 
