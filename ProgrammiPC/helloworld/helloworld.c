527f60b68b2a2d6a33e7d83ff42fe7bbfe16de92
Salah Marwan <gvs448@cam.bellinzona>
2026-09-01 11:12:58 +0200

    stampa solo i primi la prova

--- ProgrammiPC/helloworld/helloworld.c
+++ ProgrammiPC/helloworld/helloworld.c
@@ -3,8 +3,22 @@
 #include <stdio.h>
 
 int main(){
-    for(int i = 1; i <= 1000; i += 2){
-            printf("%d Hello, World!\n", i);        
+    for(int i = 1; i < 1000; i ++){
+
+        int primo = 1;
+
+        for (int j = 2; j < i; j++) {
+            
+            if (i % j == 0) {
+                primo = 0;
+                break;
+            }           
+        }
+
+        if (primo) {
+            printf("%03d Hello, World!\n", i);
+        } 
+
     }
     
 }
